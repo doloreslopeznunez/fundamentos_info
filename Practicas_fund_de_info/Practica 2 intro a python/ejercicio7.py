f29e8e2f485a0dee2e01b6398032d3420399abc8
doloreslopeznunez <101673822+doloreslopeznunez@users.noreply.github.com>
2022-04-18 16:54:16 -0300

    Delete ejercicio7.py

--- Practicas_fund_de_info/Practica 2 intro a python/ejercicio7.py
+++
@@ -1,9 +0,0 @@
-def agregar_lista():
-    numero = 0
-    
-    lista = []
-    while numero > 0:
-        numero = int(input("ingrese un num cualquiera: "))
-        lista.append(int(input("ingrese número: ")))
-
-agregar_lista()
