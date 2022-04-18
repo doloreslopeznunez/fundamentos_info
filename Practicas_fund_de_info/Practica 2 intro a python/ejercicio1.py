8c2e9f23aa09494041b4a22283b79ede26e0cb77
doloreslopeznunez <101673822+doloreslopeznunez@users.noreply.github.com>
2022-04-18 16:53:27 -0300

    Delete ejercicio1.py

--- Practicas_fund_de_info/Practica 2 intro a python/ejercicio1.py
+++
@@ -1,8 +0,0 @@
-def mayus():
-    cadena = input("Introduzca la cadena: ")
-    if str.isupper(cadena[0]):
-        print("Comienza con mayuscula")
-    else:
-        print("no comienza con mayuscula")
-
-mayus()
