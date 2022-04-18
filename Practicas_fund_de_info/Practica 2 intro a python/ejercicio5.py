b5b30e01adb1e80a0c655d1cbd6709c2e53701a8
doloreslopeznunez <101673822+doloreslopeznunez@users.noreply.github.com>
2022-04-18 16:54:05 -0300

    Delete ejercicio5.py

--- Practicas_fund_de_info/Practica 2 intro a python/ejercicio5.py
+++
@@ -1,8 +0,0 @@
-def semana(): 
-    dia = int(input("introduzca numero de dia de la semana: "))
-    lista_dias = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"]
-    if dia>7 or dia<1: 
-        print("número incorrecto")
-    else:
-        print(lista_dias[dia-1])
-semana()
