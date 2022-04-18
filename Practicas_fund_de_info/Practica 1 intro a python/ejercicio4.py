4dd5dec6570163a0d91f60b8b632c8769a2125ed
doloreslopeznunez <101673822+doloreslopeznunez@users.noreply.github.com>
2022-04-18 16:53:11 -0300

    Delete ejercicio4.py

--- Practicas_fund_de_info/Practica 1 intro a python/ejercicio4.py
+++
@@ -1,9 +0,0 @@
-#4
-
-nombre = input ('Introduzca su nombre: ')
-primer_apellido = input ('Introduzca su primer apellido: ')
-segundo_apellido = input ('Introduzca su segundo apellido: ')
-print (str.capitalize(nombre) + " " + str.capitalize(primer_apellido) + " " + str.capitalize(segundo_apellido))
-    
-
-
